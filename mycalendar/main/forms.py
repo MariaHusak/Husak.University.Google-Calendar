ca4e72ed3bc5daabb2a8ca00f7dec0a4e4754819
MariaHusak <husakmaria74@gmail.com>
2024-04-06 18:18:35 +0300

    delete

--- mycalendar/main/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['title', 'date', 'start_time', 'end_time', 'description']
-
