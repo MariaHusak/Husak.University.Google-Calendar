7175f73c9fa8f37e89d3c45aca9d5ac405f91450
MariaHusak <husakmaria74@gmail.com>
2024-04-09 16:26:19 +0300

    Added -venv

--- mycalendar/urls.py
+++ mycalendar/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-
     path('', TemplateView.as_view(template_name="main\index.html")),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
